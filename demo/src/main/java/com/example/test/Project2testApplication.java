cc5da29e94c9ae3d4ae321ab8eac42a0d6f26666
Elis Metaliaj <elismetaliaj@lhind.dlh.de>
2023-11-08 20:29:53 +0100

    helmproject-java

---
+++ demo/src/main/java/com/example/test/Project2testApplication.java
@@ -0,0 +1,13 @@
+package com.example.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project2testApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project2testApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/example/test/Project2testApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project2testApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
